b6e6f5f5ddbd8a35a6b3f29ba1bc1f3b8d7df03d
beebablaster <sanzharsyzdykov1738@gmail.com>
2025-04-29 01:45:55 +0500

    fixing routing errors

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { CartProvider } from "@/contexts/CartContext";
 
@@ -40,7 +40,7 @@ const App = () => {
             <TooltipProvider>
               <Toaster />
               <Sonner />
-              <BrowserRouter>
+              <HashRouter>
                 <Routes>
                   <Route element={<Layout />}>
                     {/* Public Routes */}
@@ -95,7 +95,7 @@ const App = () => {
                     <Route path="*" element={<NotFound />} />
                   </Route>
                 </Routes>
-              </BrowserRouter>
+              </HashRouter>
             </TooltipProvider>
           </CartProvider>
         </AuthProvider>
